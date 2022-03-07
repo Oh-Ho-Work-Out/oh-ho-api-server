9420953a1f3317861c31a02fb89599f2e435fe5c
hocaron <pooh0407@ewhain.net>
2022-03-07 15:52:07 +0900

    feat : 사용자 회원가입 및 로그인 service 생성

--- src/main/java/ohho/backend/spring/domain/member/service/MemberService.java
+++ src/main/java/ohho/backend/spring/domain/member/service/MemberService.java
@@ -1,13 +1,15 @@
 package ohho.backend.spring.domain.member.service;
 
-import java.util.Collection;
-import java.util.List;
 import ohho.backend.spring.domain.member.entities.Member;
 import ohho.backend.spring.domain.member.vo.JoinRequestVo;
+import ohho.backend.spring.domain.member.vo.LoginRequestVo;
+import ohho.backend.spring.domain.member.vo.LoginResponseVo;
 
 public interface MemberService {
 
     Member getMember(Long memberId);
 
-    Member join(JoinRequestVo joinRequestVo);
+    Member signUp(JoinRequestVo joinRequestVo);
+
+    LoginResponseVo signIn(LoginRequestVo loginRequestVo);
 }
--- src/main/java/ohho/backend/spring/domain/member/service/MemberServiceImpl.java
+++ src/main/java/ohho/backend/spring/domain/member/service/MemberServiceImpl.java
@@ -1,14 +1,20 @@
 package ohho.backend.spring.domain.member.service;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import ohho.backend.spring.config.jwt.JwtService;
 import ohho.backend.spring.domain.member.entities.Member;
-import ohho.backend.spring.domain.member.exception.ApplicantNotFoundException;
+import ohho.backend.spring.domain.member.exception.MemberNotFoundException;
+import ohho.backend.spring.domain.member.exception.MemberEmailDuplicatedException;
+import ohho.backend.spring.domain.member.exception.MemberSignUpRequestInvalidException;
 import ohho.backend.spring.domain.member.repository.MemberRepository;
 import ohho.backend.spring.domain.member.vo.JoinRequestVo;
+import ohho.backend.spring.domain.member.vo.LoginRequestVo;
+import ohho.backend.spring.domain.member.vo.LoginResponseVo;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,21 +22,58 @@ import org.springframework.util.Assert;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @Override
-    public Member getMember(Long applicantId) {
-        Assert.notNull(applicantId, "'applicantId' must not be null");
-        return memberRepository.findById(applicantId)
-            .orElseThrow(ApplicantNotFoundException::new);
+    public Member signUp(JoinRequestVo joinRequestVo) {
+        Assert.notNull(joinRequestVo, "'joinRequestVo' must not be null");
+        validate(joinRequestVo);
+
+        Member member = Member.of(
+            joinRequestVo.getEmail(),
+            passwordEncoder.encode(joinRequestVo.getPassword()),
+            joinRequestVo.getNickname()
+        );
+        // TODO: 클라쪽에서 로그인 어떻게 처리할지 논의
+        return memberRepository.save(member);
     }
 
-    @Transactional
+    private void validate(JoinRequestVo joinRequestVo) {
+        Assert.notNull(joinRequestVo, "'adminMemberVo' must not be null");
+
+        if (!StringUtils.hasText(joinRequestVo.getEmail())) {
+            throw new MemberSignUpRequestInvalidException(
+                "'username' must not be null, empty or blank");
+        }
+        if (!StringUtils.hasText(joinRequestVo.getPassword())) {
+            throw new MemberSignUpRequestInvalidException(
+                "'password' must not be null, empty or blank");
+        }
+        if (!StringUtils.hasText(joinRequestVo.getNickname())) {
+            throw new MemberSignUpRequestInvalidException(
+                "'nickname' must not be null, empty or blank");
+        }
+        if (memberRepository.existsByEmail(joinRequestVo.getEmail())) {
+            throw new MemberEmailDuplicatedException(
+                "이미 사용중인 email 입니다. email: " + joinRequestVo.getEmail());
+        }
+    }
+
     @Override
-    public Member join(JoinRequestVo joinRequestVo) {
-        Assert.notNull(joinRequestVo, "'joinRequestVo' must not be null");
-        return memberRepository.findByEmail(joinRequestVo.getPassword())
-            .orElseGet(() -> memberRepository.save(
-                Member.of(joinRequestVo.getEmail(), joinRequestVo.getPassword()))
-            );
+    public LoginResponseVo signIn(LoginRequestVo loginRequestVo) {
+        Member member = memberRepository.findByEmailAndPassword(loginRequestVo.getEmail(),
+                passwordEncoder.encode(loginRequestVo.getPassword()))
+            .orElseThrow(MemberNotFoundException::new);
+        return LoginResponseVo.of(
+            jwtService.encode(member.getId()),
+            member
+        );
+    }
+
+    @Override
+    public Member getMember(Long memberId) {
+        Assert.notNull(memberId, "'memberId' must not be null");
+        return memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
     }
 }
