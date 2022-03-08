638e777d7ee1764e08ee7c8a683b6a530fe3e98d
hocaron <pooh0407@ewhain.net>
2022-03-08 20:56:03 +0900

    refactor : base entity 에서 상속 받도록 변경

--- src/main/java/ohho/backend/spring/domain/member/entities/Member.java
+++ src/main/java/ohho/backend/spring/domain/member/entities/Member.java
@@ -1,16 +1,10 @@
 package ohho.backend.spring.domain.member.entities;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
@@ -18,22 +12,12 @@ import lombok.Getter;
 import lombok.ToString;
 import ohho.backend.spring.common.entities.BaseEntity;
 import ohho.backend.spring.domain.exerciseHistory.entities.ExerciseHistory;
-import ohho.backend.spring.domain.group.entities.Group;
+import ohho.backend.spring.domain.organization.entities.Organization;
 import ohho.backend.spring.domain.partner.entities.Partner;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
-@ToString(of = {"id"})
-@EqualsAndHashCode(of = "id")
-@EntityListeners(AuditingEntityListener.class)
 public class Member extends BaseEntity {
-
-    @Id
-    @GeneratedValue
-    private Long id;
 
     private String email;
 
@@ -46,24 +30,13 @@ public class Member extends BaseEntity {
     /* 연관관계 */
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "partner_id")
     private Partner partner;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
-    private Group group;
+    private Organization organization;
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
     private List<ExerciseHistory> exerciseHistories = new ArrayList<>();
-
-    /* date 컬럼 */
-
-    @CreatedDate
-    @Column(updatable = false, nullable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
     protected Member() {
     }
--- src/main/java/ohho/backend/spring/domain/partner/entities/Partner.java
+++ src/main/java/ohho/backend/spring/domain/partner/entities/Partner.java
@@ -1,31 +1,21 @@
 package ohho.backend.spring.domain.partner.entities;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ohho.backend.spring.common.entities.BaseEntity;
 import ohho.backend.spring.domain.member.entities.Member;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Partner extends BaseEntity {
-
-    @Id
-    @GeneratedValue
-    private Long id;
 
     private LocalDate dDay;
 
@@ -35,13 +25,4 @@ public class Partner extends BaseEntity {
 
     @OneToMany(mappedBy = "partner", cascade = CascadeType.ALL)
     private List<Member> members = new ArrayList<>();
-
-    /* date 컬럼 */
-
-    @CreatedDate
-    @Column(updatable = false, nullable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
