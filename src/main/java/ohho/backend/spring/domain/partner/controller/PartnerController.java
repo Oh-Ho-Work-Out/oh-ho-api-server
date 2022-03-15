bdd26517d95adf54df08c8db548578f0687d4522
hocaron <pooh0407@ewhain.net>
2022-03-15 21:53:56 +0900

    feat : 파트너 요청 및 수락 구현

--- src/main/java/ohho/backend/spring/domain/partner/controller/PartnerController.java
+++ src/main/java/ohho/backend/spring/domain/partner/controller/PartnerController.java
@@ -4,11 +4,16 @@ package ohho.backend.spring.domain.partner.controller;
 import lombok.RequiredArgsConstructor;
 import ohho.backend.spring.common.ApiResponse;
 import ohho.backend.spring.domain.partner.entities.Partner;
-import ohho.backend.spring.domain.partner.model.request.UpdateDdayAndGoalRequest;
+import ohho.backend.spring.domain.partner.exception.PartnerNotFoundException;
+import ohho.backend.spring.domain.partner.model.request.AcceptPartnerRequestDto;
+import ohho.backend.spring.domain.partner.model.request.RequestPartnerRequestDto;
+import ohho.backend.spring.domain.partner.model.request.UpdateDdayAndGoalRequestDto;
 import ohho.backend.spring.domain.partner.service.PartnerService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,20 @@ public class PartnerController {
 
     private final PartnerService partnerService;
 
+    @PostMapping("/request")
+    public ApiResponse<String> requestPartner(@ModelAttribute("memberId") Long memberId,
+        @RequestBody RequestPartnerRequestDto requestPartnerRequestDto) {
+        return ApiResponse.success(
+            partnerService.requestPartner(memberId, requestPartnerRequestDto));
+    }
+
+    @PostMapping("/accept")
+    public ApiResponse<Partner> acceptPartner(@ModelAttribute("memberId") Long memberId,
+        @RequestBody AcceptPartnerRequestDto acceptPartnerRequestDto) {
+        return ApiResponse.success(
+            partnerService.acceptPartner(memberId, acceptPartnerRequestDto));
+    }
+
     @GetMapping("/{id}")
     public ApiResponse<Partner> getPartner(@PathVariable("id") Long id) {
         return ApiResponse.success(partnerService.getPartner(id));
@@ -28,7 +47,7 @@ public class PartnerController {
     @PatchMapping("/{id}")
     public ApiResponse<String> updateDdayAndGoalRequest(
         @PathVariable("id") Long id,
-        @RequestBody UpdateDdayAndGoalRequest updateDdayAndGoalRequest) {
+        @RequestBody UpdateDdayAndGoalRequestDto updateDdayAndGoalRequest) {
         return ApiResponse.success(partnerService.updateDdayAndGoal(id, updateDdayAndGoalRequest));
     }
 
---
+++ src/main/java/ohho/backend/spring/domain/partner/model/request/RequestPartnerRequestDto.java
@@ -0,0 +1,9 @@
+package ohho.backend.spring.domain.partner.model.request;
+
+import lombok.Data;
+
+@Data
+public class RequestPartnerRequestDto {
+
+    private long receiverId;
+}
