6076248df1631eb12329090e48593be150d971d6
hocaron <pooh0407@ewhain.net>
2022-04-25 15:42:17 +0900

    test : OhhoApiApplicationTest 생성

---
+++ ohho-api/src/test/java/kr/ohho/OhhoApiApplicationTest.java
@@ -0,0 +1,14 @@
+package kr.ohho;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhhoApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
